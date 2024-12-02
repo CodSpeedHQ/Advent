42b6a81e38b37a9cab0d9792a2b8133ab7ebb7ce
Arthur Pastel <arthur.pastel@gmail.com>
2024-12-02 17:45:09 +0100

    feat: add GH action day runner

---
+++ manager/src/manager/handlers/run_all.py
@@ -0,0 +1,24 @@
+import sys
+from manager.github import GITHUB_ADVENT_REPO
+from manager.repository_repository import RepositoryRepository
+
+SCHEDULE_RUN_WORKFLOW = GITHUB_ADVENT_REPO.get_workflow("schedule-run.yml")
+
+
+def main(day: str):
+    repos = RepositoryRepository.find_all()
+    for repo in repos:
+        print(f"Running {repo.owner}/{repo.name} for day {day}")
+        r = SCHEDULE_RUN_WORKFLOW.create_dispatch(ref="main-private", inputs={
+            "repo_fullname": f"{repo.owner}/{repo.name}",
+            "toolchain": repo.toolchain,
+            "crate": repo.crate,
+            "day": day
+        })
+        if not r:
+            print(f"Failed to run {repo.owner}/{repo.name}")
+
+if __name__ == "__main__":
+    day = sys.argv[1]
+    print(f"Running all repos for day {day}")
+    main(day)
