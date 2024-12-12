26e3e7bebdb7adfdcd584aabd926c468c96006be
Arthur Pastel <arthur.pastel@gmail.com>
2024-12-12 11:14:11 +0100

    feat: improve solution conformity check reliability

---
+++ rust-runner/test_build.rs
@@ -0,0 +1,22 @@
+/// This file is used to test that the solution compiles and actually contains day<day_number> part1 and part2 functions
+use rust_runner::check_result;
+
+paste::paste! {
+    use solution::[<day env!("DAY_NUMBER")>]::{part1, part2};
+}
+
+#[test]
+fn test_build_part_1() {
+    let input = "PLACEHOLDER";
+    let output = part1(input);
+    let expected = "PLACEHOLDER";
+    check_result(output, expected, 1);
+}
+
+#[test]
+fn test_build_part_2() {
+    let input = "PLACEHOLDER";
+    let output = part2(input);
+    let expected = "PLACEHOLDER";
+    check_result(output, expected, 2);
+}
