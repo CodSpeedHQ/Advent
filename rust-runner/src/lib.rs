0ec419ba3524a8c628cb30fa3410420017cdf3ba
Arthur Pastel <arthur.pastel@gmail.com>
2024-12-17 13:27:33 +0100

    feat: restrict output types

--- rust-runner/src/lib.rs
+++ rust-runner/src/lib.rs
@@ -1,4 +1,19 @@
-pub fn check_result(output: impl ToString, expected: &str, part: u8) {
+pub trait Output: ToString {}
+
+macro_rules! allow_output_types {
+    ($($t:ty),*) => {
+        $(impl Output for $t {})*
+    };
+}
+
+allow_output_types!(
+    u8, u16, u32, u64, u128, usize, // Unsigned integers
+    i8, i16, i32, i64, i128, isize, // Signed integers
+    f32, f64, // Floats
+    String, &str // Strings
+);
+
+pub fn check_result(output: impl Output, expected: &str, part: u8) {
     if output.to_string().trim() != expected.trim() {
         panic!("Output does not match expected for part {}", part);
     }
